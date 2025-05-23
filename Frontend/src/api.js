d8d9bfff8108de78f00512e959245fe07596c2cf
Narendra <141820488+Narendrachillal@users.noreply.github.com>
2025-05-23 10:55:27 +0530

    Update api.js

--- Frontend/src/api.js
+++ Frontend/src/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = "http://localhost:5000/api/v1";
+const API_BASE_URL = "https://todo-backend-qfx5.onrender.com";
 
 export const api = axios.create({
   baseURL: API_BASE_URL,
